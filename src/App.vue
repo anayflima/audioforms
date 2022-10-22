8671be5eacc37a0d2ea087577708f0c2f7ac71c5
Ana Lima <ayf.lima@gmail.com>
2022-10-22 15:29:16 -0300

    Delete commented lines

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,5 @@
 <template>
 
-  <!-- <p>OLAAAA</p>
-
-   <form action="/action_page.php">
-			<label for="name">Nome:</label><br>
-			<input type="text" id="name" name="name" value=""><br><br>
-
-			<label for="age">Idade:</label><br>
-			<input type="text" id="age" name="age" value=""><br><br>
-
-			<label for="curso">Curso:</label><br>
-			<input type="text" id="curso" name="curso" value=""><br><br>
-			<input type="submit" value="Submit">
-		</form>  -->
-
     <v-container class="pl-10">
     <v-btn color="primary" elevation="1" small @click="ToggleMic"></v-btn>
     
@@ -39,10 +25,7 @@
 
 <script>
 
-// import { ref } from 'vue'
-
 const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition
-// const Synthesis = window.speechSynthesis;
 const sr = new Recognition()
 let i = 0
 
@@ -76,23 +59,8 @@ export default {
     }),
 
     mounted() {
-        console.log("ola")
-        // const field = document.querySelector(this.selectors_array[0])
-        // let field = this.$el.querySelector("#Name").value;
-        // console.dir(field)
-        // console.log("field")
-        // console.log(field)
-
-        console.log('this')
-        console.log(this)
-
-        // this.Recognition = window.SpeechRecognition || window.webkitSpeechRecognition
-
         console.log(sr)
-
-        
-        // sr =  new Recognition()
-        
+              
         sr.lang = 'pt-BR';
         sr.continuous = false
 
@@ -110,9 +78,6 @@ export default {
 
         sr.onresult = (evt) => {
 
-          // console.log('evt.results')
-          // console.log(evt.results)
-
           for (let i = 0; i < evt.results.length; i++) {
             const result = evt.results[i]
 
@@ -124,8 +89,6 @@ export default {
           const t = evt_results_array[evt_results_array.length -1][0].transcript
 
           console.log(t)
-
-          // console.log("i = ", i)
 
           this.nome = t
 
@@ -151,8 +114,7 @@ export default {
 
         },
         ToggleMic() {
-          // Vamos iniciar o preenchimento do forms
-          this.speakSentence("Olá!").then(() => {
+          this.speakSentence("Olá! Vamos iniciar o preenchimento do forms").then(() => {
             this.speakSentence(this.fields_array[0]).then(() => {
               console.log('FIM SPEECH')
               console.log('sr')
@@ -164,29 +126,10 @@ export default {
               }
             })
           })
-          // await speakSentence("Olá! Vamos iniciar o preenchimento do forms")
-          // console.log('FIM SPEECH')
           
           
         },
         CheckForCommand () {
-            
-          //   const t = result[0].transcript;
-          //   if (t.includes('stop recording')) {
-          //     console.log('STOP RECORDING')
-          //     sr.stop()
-          //   } else if (
-          //     t.includes('what is the time') ||
-          //     t.includes('what\'s the time')
-          //   ) {
-          //     sr.stop()
-          //     alert(new Date().toLocaleTimeString())
-          //     setTimeout(() => sr.start(), 100)
-          //   }
-          // }
-
-          // console.log('result')
-          // console.log(result)
 
           console.log("IF ACABOU FORMS com i = ", i)
 
